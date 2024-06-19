0cb39af0cf9a16d1782ef8acc589c8f0109f4acc
Jonathan vera <167922770+jonathanavl@users.noreply.github.com>
2024-06-19 11:16:17 +0000

    First commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+        <footer className="bg-dark text-center text-lg-start">
+        <div className="text-center p-3 text-light">
+          © 2020 Copyright: MDBootstrap.com
+        </div>
+      </footer>
+    );
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,25 +2,34 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import {Card} from "./card.jsx";
+import {NavBar} from "./navbar.jsx";
+import {Jumbotron} from './Jumbotron';
+import {Footer} from './Footer';
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<div>
+		<NavBar />
+		<div class="container">
+			<br />
+		<Jumbotron/>
+		<br />
+		<div className="d-flex justify-content-around">
+			<br />
+			<Card /> 
+			<Card /> 
+			<Card /> 
+			<Card />
+			</div>
+			<br />
 		</div>
-	);
+		<br />
+		<br />
+		<Footer/>
+	</div>
+);
 };
 
 export default Home;
